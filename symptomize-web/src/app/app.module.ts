9994dd25171112e4aa888d208e65d79cd7c0d107
Noah Salvi <noahsalvi@me.com>
2020-06-17 17:39:48 +0200

    add working maphighlight

--- symptomize-web/src/app/app.module.ts
+++ symptomize-web/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LandingComponent } from "./landing/landing.component";
-import { SymptomizeComponent } from './symptomize/symptomize.component';
-import { QuittungComponent } from './quittung/quittung.component';
+import { SymptomizeComponent } from "./symptomize/symptomize.component";
+import { QuittungComponent } from "./quittung/quittung.component";
+import { MaphilightModule } from "ng-maphilight";
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, SymptomizeComponent, QuittungComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    SymptomizeComponent,
+    QuittungComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, MaphilightModule],
   providers: [],
   bootstrap: [AppComponent],
 })
