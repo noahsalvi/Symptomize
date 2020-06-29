fa67d253c0816b3540d5923a1f25e44fade92615
Noah Salvi <noahsalvi@me.com>
2020-06-29 19:42:40 +0200

    finish all functionality on symptomize

--- symptomize-web/src/app/quittung/quittung.component.ts
+++ symptomize-web/src/app/quittung/quittung.component.ts
@@ -1,36 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { Location } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Local } from 'protractor/built/driverProviders';
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
+import { Location } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Local } from "protractor/built/driverProviders";
+import { SymptomService } from "../symptom.service";
 
 @Component({
-  selector: 'app-quittung',
-  templateUrl: './quittung.component.html',
-  styleUrls: ['./quittung.component.scss']
+  selector: "app-quittung",
+  templateUrl: "./quittung.component.html",
+  styleUrls: ["./quittung.component.scss"],
 })
 export class QuittungComponent implements OnInit {
-
   public URLoutput;
-  public urlLabeltext = 'Hier wird der Link erscheinen, wenn sie auf absenden clicken';
+  public urlLabeltext =
+    "Hier wird der Link erscheinen, wenn sie auf absenden clicken";
   public showcopy = false;
+  private dictionary;
 
+  symptoms = [];
 
-  symptome = [
-    { id: 1, koerperteil: 'Hand', Symptom: ['Bluten', 'Blau'] },
-    { id: 2, koerperteil: 'Kopf', Symptom: ['Schnitt', 'Blau', 'LOL'] }
-  ];
-
-  constructor(private router: Router, private location: Location, private http: HttpClient) {
-
+  constructor(
+    private router: Router,
+    private location: Location,
+    private http: HttpClient,
+    private symptomService: SymptomService
+  ) {
+    this.symptoms = this.symptomService.profile;
+    this.dictionary = this.symptomService.dictionary;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   clickPlus() {
-    this.router.navigate(['/symp']);
+    this.router.navigate(["/symp"]);
   }
   clickEdit(id: number) {
     alert(id);
@@ -39,38 +42,38 @@ export class QuittungComponent implements OnInit {
     alert(id);
   }
   absenden() {
-    const url = 'http://localhost:8080/saveProfil';
-    if (confirm('Bist du sicher, dass du dein Profil absenden willst?')) {
-      this.http.post<Observable<Text>>(url, {
-          symptome: this.symptome
-      })
+    const url = "http://localhost:8080/saveProfil";
+    if (confirm("Bist du sicher, dass du dein Profil absenden willst?")) {
+      this.http
+        .post<Observable<Text>>(url, {
+          symptoms: this.symptoms,
+        })
         .subscribe((output) => {
           this.URLoutput = output;
           console.log(this.URLoutput);
           this.giveURL(this.URLoutput);
           this.showcopy = true;
-
         });
     }
   }
   clickCopy(val: string) {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
+    const selBox = document.createElement("textarea");
+    selBox.style.position = "fixed";
+    selBox.style.left = "0";
+    selBox.style.top = "0";
+    selBox.style.opacity = "0";
     selBox.value = val;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(selBox);
   }
   goBack() {
-    alert('back');
+    alert("back");
     this.location.back();
   }
   giveURL(url: Text) {
-    this.urlLabeltext = 'http://localhost:4200/profil/' + url;
+    this.urlLabeltext = "http://localhost:4200/profil/" + url;
   }
 }
