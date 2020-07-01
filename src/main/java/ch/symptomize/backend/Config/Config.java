e2f1b94efde6ab68544dc361c2caf62d2a1aa876
Noah Salvi <noahsalvi@me.com>
2020-07-01 22:37:39 +0200

    fix quittung mobile and try routing fix

---
+++ src/main/java/ch/symptomize/backend/Config/Config.java
@@ -0,0 +1,26 @@
+package ch.symptomize.backend.Config;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@Configuration
+public class Config {
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return new ErrorViewResolver() {
+            @Override
+            public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+                return status == HttpStatus.NOT_FOUND
+                        ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+                        : null;
+            }
+        };
+    }
+}
